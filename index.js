18266e2adeb9ffec1112f70c4afa05705b7f711e
Anshika <anshikapathak0802@gmail.com>
2026-06-15 15:38:47 +0530

    Recommit

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log("Arigato Claude!")
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/yourusername/shopeasy-clothing-store.git
+git push -u origin main
