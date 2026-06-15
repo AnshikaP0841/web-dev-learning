5e957709721ee4297dab64165297a7d7ecbde07a
Anshika <anshikapathak0802@gmail.com>
2026-06-15 17:01:12 +0530

    A pratice

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-console.log("Arigato Claude!")
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/yourusername/shopeasy-clothing-store.git
-git push -u origin main
+console.log("Hello World!")
